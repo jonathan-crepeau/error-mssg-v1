dd5a9608b5dcae61871fc6e784d518597389c7cc
jonathan-crepeau <jon.crepeau@gmail.com>
2023-02-18 20:58:36 -0800

    error messaging v1 complete

--- app.js
+++ app.js
@@ -1,8 +1,45 @@
 const btn = document.getElementById('signup-btn');
+const form = document.getElementById('signup-form');
+const inputs = document.querySelectorAll('.input-field');
+
+inputs.forEach((input) => {
+    input.addEventListener('input', handleWriting);
+});
 
 btn.addEventListener('click', handleClick);
+// formInput.addEventListener('click', handleWriting);
+
+/*!SECTION
+1. get all input elements
+
+*/
+
 
 function handleClick(event) {
     event.preventDefault();
-    console.log('clicked!');
+    console.log('button clicked!');
+    document.querySelectorAll('.alert').forEach((alert) => alert.remove());
+
+    const formInputs = [...form.elements];
+    formInputs.forEach((input) => {
+        if (input.type !== 'button' && input.value === '') {
+            input.classList.add('input-error');
+            if (!input.nextElementSibling.classList.contains('alert')) {
+                input.insertAdjacentHTML('afterend', `
+                <div class="alert ${input.id}-message">
+                    Please input your ${input.placeholder}
+                </div>
+            `);
+            }
+        }
+    })
+}
+
+
+function handleWriting(event) {
+    event.preventDefault();
+    event.target.classList.remove('input-error');
+    if (document.querySelector(`.${event.target.id}-message`)) {
+        document.querySelector(`.${event.target.id}-message`).remove();
+    }
 }
